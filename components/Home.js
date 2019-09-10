af59e3d378b87f6ae8cd58b1ff68468c063688df
quan2031 <quan2031@gmail.com>
2019-09-10 17:50:48 +0900

    2 files modified

--- components/Home.js
+++ components/Home.js
@@ -2,13 +2,14 @@ import React from 'react'
 // import { connect } from 'react-redux'
 import { useDispatch } from 'react-redux'
 import NewsItems from '../containers/NewsItems'
+import Loading from '../containers/Loading'
 import { getNews } from '../actions'
 
 //sử dụng họok thay vì dùng connect của redux 
 const Home = () =>{
   const dispatch = useDispatch();
   dispatch({ type: 'GET_NEWS' })
-   return <NewsItems />
+   return [<NewsItems />, <Loading/>]
 }
 export default Home
 
--- containers/Loading.js
+++ containers/Loading.js
@@ -1,14 +1,27 @@
 import React from 'react';
-import { connect } from 'react-redux'
+import { connect, useSelector } from 'react-redux'
 import img from './giphy.gif'
-let Loading = ({ loading }) => (
-loading ?
-<div style={{ textAlign: 'center' }}>
-   <img src={img} alt='loading' />
-   <h1>LOADING</h1>
-</div>:
-null
-);
-const mapStateToProps = (state) => ({loading: state.loading})
-Loading = connect(mapStateToProps,null)(Loading)
-export default Loading;
+
+export default () =>{
+  const loading = useSelector(state => state.loading)
+  return(
+    loading ?
+    <div style={{ textAlign: 'center' }}>
+      <img src={img} alt='loading' />
+      <h1>LOADING</h1>
+    </div>:
+    null
+  )
+}
+
+// let Loading = ({ loading }) => (
+// loading ?
+// <div style={{ textAlign: 'center' }}>
+//    <img src={img} alt='loading' />
+//    <h1>LOADING</h1>
+// </div>:
+// null
+// );
+// const mapStateToProps = (state) => ({loading: state.loading})
+// Loading = connect(mapStateToProps,null)(Loading)
+// export default Loading;
